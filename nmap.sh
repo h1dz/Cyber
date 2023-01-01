98572b42454df0868bf91b65d8328f933a3a03ce
h1dz <85143916+h1dz@users.noreply.github.com>
2023-01-01 18:05:04 +1100

    Create nmap.sh

---
+++ nmap.sh
@@ -0,0 +1,64 @@
+#!/bin/bash
+GF='\033[5;32m' #${GF} 
+G='\033[0;92m' #${G}  
+Y='\033[0;33m' #${Y}
+N='\033[0m'    #${N}
+rm -rf NMAPscanTCPfull.txt 2>/dev/null
+rm -rf NMAPscanTCPq.txt 2>/dev/null
+rm -rf NMAPscanUDPq.txt 2>/dev/null
+rm -rf NMAPscanUDPfull.txt 2>/dev/null
+clear
+#TCP
+#quick
+tput bold; printf "${GF}IP = "$1
+tput bold; printf "\n${Y}[+] Starting Quick TCP Scan at: ${N}" | tee -a -i NMAPscanTCPq.txt
+date | tee -a -i NMAPscanTCPq.txt
+sudo nmap -Pn -T5 --top-ports 5000 --min-rate 100 -n --open $1 --max-retries 1 --defeat-rst-ratelimit -oN NMAPscanTCPq.txt 2>/dev/null
+clear 
+tput bold; printf "${GF}IP = "$1
+tput bold; printf "\n${G}[-] Quick TCP Scan Completed at: ${N}\n" | tee -a -i NMAPscanTCPq.txt
+date | tee -a -i NMAPscanTCPq.txt
+cat NMAPscanTCPq.txt
+tput bold; printf "${Y}[+] Starting Full TCP Scan at: ${N}"
+#normal
+date  | tee -a -i NMAPscanTCPfull.txt
+ports1=$(sudo nmap -p- -T3 --open $1 | grep ^[0-9] | cut -d '/' -f1 | tr '\n' ',' | sed s/,$//); sudo nmap -sC -sV -p$ports1 --script=vuln $1 -oN NMAPscanTCPfull.txt 2>/dev/null
+clear 
+tput bold; printf "${GF}IP = "$1
+tput bold; printf "\n${G}[-] Full TCP Scan Completed at: ${N}\n" | tee -a -i NMAPscanTCPfull.txt
+date  | tee -a -i NMAPscanTCPfull.txt
+tput bold; printf "${Y}[+] Starting Quick UDP Scan at: ${N}"
+#UDP
+#quick
+date  | tee -a -i NMAPscanUDPq.txt
+sudo nmap -sU -n --top-ports 5000 -T5 --open $1 --max-retries 1 --defeat-rst-ratelimit -oN NMAPscanUDPq.txt
+clear 
+tput bold; printf "${GF}IP = "$1
+tput bold; printf "\n${G}[-] Quick UDP Scan Completed at: ${N}" | tee -a -i NMAPscanUDPq.txt
+date  | tee -a -i NMAPscanUDPq.txt
+tput bold; printf "${Y}[+] Starting Full UDP Scan at: ${N}"
+#normal
+date  | tee -a -i NMAPscanUDPfull.txt
+ports3=$(sudo nmap -sU -p- -T5 --open $1 | grep ^[0-9] | cut -d '/' -f1 | tr '\n' ',' | sed s/,$//)
+if [ -z "$ports3" ]
+then
+tput bold; printf "${G}[+] NMAP script complete! Displaying the full TCP results, no open UDP ports found:${N}"
+sleep 2
+clear
+cat NMAPscanTCPfull.txt
+tput bold; printf "${GF}Run this scan again to confirm output!${N}"
+sudo rm -rf NMAPscanUDPq.txt
+exit 1
+else
+sudo nmap -sU -sV -p$ports3 --script=vuln $1 -oN NMAPscanUDPfull.txt 2>/dev/null
+clear 
+tput bold; printf "${GF}IP = "$1
+tput bold; printf "\n${G}[-] Full UDP Scan Completed at: ${N}" | tee -a -i NMAPscanUDPfull.txt
+date  | tee -a -i NMAPscanUDPfull.txt
+tput bold; printf "${G}[+] NMAP script complete! Displaying the full TCP & UDP results:${N}"
+sleep 2
+clear
+cat NMAPscanTCPfull.txt
+cat NMAPscanUDPfull.txt
+tput bold; printf "${GF}Run this scan again to confirm output!${N}"
+fi
