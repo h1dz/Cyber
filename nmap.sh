82d8d6ffd58745c762cb7bfd304096c3dee2e831
h1dz <85143916+h1dz@users.noreply.github.com>
2023-01-04 00:58:21 +1100

    Update nmap.sh

--- nmap.sh
+++ nmap.sh
@@ -15,10 +15,10 @@ date | tee -a -i NMAPscanTCPq.txt
 sudo nmap -Pn -T5 --top-ports 5000 --min-rate 100 -n --open $1 --max-retries 1 --defeat-rst-ratelimit -oN NMAPscanTCPq.txt 2>/dev/null
 clear 
 tput bold; printf "${GF}IP = "$1
-tput bold; printf "\n${G}[-] Quick TCP Scan Completed at: ${N}\n" | tee -a -i NMAPscanTCPq.txt
+tput bold; printf "\n${G}[-] Quick TCP Scan Completed at: ${N}" | tee -a -i NMAPscanTCPq.txt
 date | tee -a -i NMAPscanTCPq.txt
 cat NMAPscanTCPq.txt
-tput bold; printf "${Y}[+] Starting Full TCP Scan at: ${N}" | tee -a -i NMAPscanTCPq.txt
+tput bold; printf "\n\n${Y}[+] Starting Full TCP Scan at: ${N}" | tee -a -i NMAPscanTCPfull.txt
 date  | tee -a -i NMAPscanTCPfull.txt
 echo " \n"  | tee -a -i NMAPscanTCPfull.txt
 ports1=$(sudo nmap -p- -T3 --open $1 | grep ^[0-9] | cut -d '/' -f1 | tr '\n' ',' | sed s/,$//); sudo nmap -sC -sV -p$ports1 --script=vuln $1 -oN NMAPscanTCPfull.txt 2>/dev/null
@@ -35,7 +35,7 @@ clear
 tput bold; printf "${GF}IP = "$1
 tput bold; printf "\n${G}[-] Quick UDP Scan Completed at: ${N}" | tee -a -i NMAPscanUDPq.txt
 date  | tee -a -i NMAPscanUDPq.txt
-tput bold; printf "${Y}[+] Starting Full UDP Scan at: ${N}" | tee -a -i NMAPscanUDPfull.txt
+tput bold; printf "\n\n${Y}[+] Starting Full UDP Scan at: ${N}" | tee -a -i NMAPscanUDPfull.txt
 date  | tee -a -i NMAPscanUDPfull.txt
 echo " \n"  | tee -a -i NMAPscanUDPfull.txt
 ports3=$(sudo nmap -sU -p- -T5 --open $1 | grep ^[0-9] | cut -d '/' -f1 | tr '\n' ',' | sed s/,$//)
@@ -44,6 +44,8 @@ then
 tput bold; printf "${G}[+] NMAP script complete! Displaying the full TCP results, no open UDP ports found:${N}"
 sleep 2
 clear
+tput bold; printf "${GF}IP = "$1
+echo " \n"
 cat NMAPscanTCPfull.txt
 tput bold; printf "${GF}Run this scan again to confirm output!${N}"
 sudo rm -rf NMAPscanUDPq.txt
@@ -57,6 +59,8 @@ date  | tee -a -i NMAPscanUDPfull.txt
 tput bold; printf "${G}[+] NMAP script complete! Displaying the full TCP & UDP results:${N}"
 sleep 2
 clear
+tput bold; printf "${GF}IP = "$1
+echo " \n"
 cat NMAPscanTCPfull.txt
 echo " \n"
 cat NMAPscanUDPfull.txt
