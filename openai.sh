1c76ee13c2e069d706e5575a72a14b011aa5573d
h1dz <85143916+h1dz@users.noreply.github.com>
2023-03-11 23:06:35 +1100

    Update openai.sh

--- openai.sh
+++ openai.sh
@@ -2,15 +2,18 @@
 G='\033[0;92m' #${G}
 N='\033[0m'    #${N}
 Y='\033[0;33m' #${Y}
-printf "${G}Ask ChatGPT: ${N}"
-read var1
-printf "${Y}"
-response=$(curl -s https://api.openai.com/v1/chat/completions \
- -H "Authorization: Bearer <YOUR-API-KEY>" \
- -H "Content-Type: application/json" \
- -d '{
- "model": "gpt-3.5-turbo",
- "messages": [{"role": "user", "content": "'"$var1"'"}] 
- }')
-content=$(echo "$response" | sed 's/^\\n\\n//' | grep -oP '(?<="content":")[^"]*')
-printf "${Y}${content}${N}\n"
+
+while true; do
+  printf "${G}Ask ChatGPT: ${N}"
+  read var1
+  printf "${Y}"
+  response=$(curl -s https://api.openai.com/v1/chat/completions \
+   -H "Authorization: Bearer <YOUR-API-KEY>" \
+   -H "Content-Type: application/json" \
+   -d '{
+   "model": "gpt-3.5-turbo",
+   "messages": [{"role": "user", "content": "'"$var1"'"}] 
+   }')
+  content=$(echo "$response" | sed 's/^\\n\\n//' | grep -oP '(?<="content":")[^"]*')
+  printf "${Y}${content}${N}\n"
+done
